d49a710243802cbd2586a485f9dcb627c71785ac
Geri <gregor.horvath83@gmail.com>
2020-05-26 22:02:31 +0300

    commit

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Counter.css';
+import './Counter.scss';
 
 const Counter = ({ title, isVisible, onButtonClick }) => {
   return isVisible ? (
